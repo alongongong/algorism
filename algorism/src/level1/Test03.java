78c0c10e4619bf47325af3c380bcbb623c7f4ece
alongongong <al_ong@naver.com>
2022-01-06 15:40:30 +0900

    1단계

---
+++ algorism/src/level1/Test03.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Test03 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
